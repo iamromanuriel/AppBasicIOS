10c818d81fdbfa4dac1f5a55bfe927c83af64143
romanuriel <romanufrancisicomartinez@gmail.com>
2024-12-31 13:11:34 -0600

    Initial Commit

---
+++ AppBasicIOS/AppBasicIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBasicIOSApp.swift
+//  AppBasicIOS
+//
+//  Created by Roman Uriel on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppBasicIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppBasicIOSTests/AppBasicIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppBasicIOSTests.swift
+//  AppBasicIOSTests
+//
+//  Created by Roman Uriel on 31/12/24.
+//
+
+import Testing
+@testable import AppBasicIOS
+
+struct AppBasicIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
